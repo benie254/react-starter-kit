853275d479b30a7e0bb3ddabc31a41e642c7ab5e
benie254 <davinci.monalissa@gmail.com>
2025-01-03 09:55:14 +0300

    chore (app): configure home route

--- react-template/src/App.tsx
+++ react-template/src/App.tsx
@@ -1,11 +1,16 @@
-import './App.scss'
+import { Route, Routes } from "react-router-dom";
+
+import "./App.scss";
+import HomeComponent from "./components/Home";
 
 function App() {
   return (
     <>
-      <h1>Vite + React + SCSS</h1>
+      <Routes>
+        <Route path='/' element={<HomeComponent />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ react-template/src/components/Home.tsx
@@ -0,0 +1,7 @@
+export default function HomeComponent() {
+  return (
+    <div>
+      <h1>Vite + React + SCSS</h1>
+    </div>
+  );
+}
