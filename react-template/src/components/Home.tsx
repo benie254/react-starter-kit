1e75ba072e19ac829c7924c4ab3a8d9f413482de
benie254 <davinci.monalissa@gmail.com>
2025-01-04 20:28:56 +0300

    change (home): update home page content

--- react-template/src/components/Home.tsx
+++ react-template/src/components/Home.tsx
@@ -1,7 +1,23 @@
 export default function HomeComponent() {
   return (
     <div>
-      <h1>Vite + React + SCSS</h1>
+      <h1>React Starter Pack</h1>
+      <p>
+        Use this template repository as a starter pack <br /> for building
+        projects in{" "}
+        <code>
+          <b>React</b>
+        </code>{" "}
+        <br />
+        with{" "}
+        <code>
+          <b>TypeScript</b>
+        </code>{" "}
+        and{" "}
+        <code>
+          <b>SCSS</b>
+        </code>
+      </p>
     </div>
   );
 }
